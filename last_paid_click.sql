56ea06453263d8263b42333147a8336de16ded7f
Natallia_Varabyova <133032557+NatalliaVarabyova@users.noreply.github.com>
2023-08-30 23:06:56 +0300

    Step 2
    Corrected mistakes

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,9 +1,8 @@
-/* Step 2. Create */
+/* Step 2.1. View creation */
 
-
-CREATE VIEW last_paid_click AS
-WITH tab AS (
-    SELECT DISTINCT ON (s.visitor_id)
+CREATE VIEW last_paid_click_nvorobyova AS
+WITH tab1 AS (
+    SELECT
         s.visitor_id,
         s.visit_date,
         s.source AS utm_source,
@@ -17,10 +16,33 @@ WITH tab AS (
     FROM sessions AS s
     LEFT JOIN leads AS l
         ON s.visitor_id = l.visitor_id
-    WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg')
-    ORDER BY s.visitor_id ASC, s.visit_date DESC
+),
+
+tab2 AS (
+    SELECT DISTINCT ON (a.visitor_id)
+        a.visitor_id,
+        a.visit_date,
+        a.utm_source,
+        b.utm_campaign,
+        b.lead_id,
+        b.created_at,
+        b.amount,
+        b.closing_reason,
+        b.status_id,
+        coalesce(b.utm_medium, 'organic') AS utm_medium
+    FROM tab1 AS a
+    LEFT JOIN tab1 AS b
+        ON a.visitor_id = b.visitor_id AND b.utm_medium != 'organic'
+    ORDER BY a.visitor_id ASC, b.visit_date DESC, a.visit_date DESC
 )
 
 SELECT *
-FROM tab
-ORDER BY visit_date, utm_source, utm_medium, utm_campaign;
+FROM tab2
+ORDER BY date_trunc('day', visit_date), utm_source, utm_medium, utm_campaign;
+
+/*Step 2.2. Selection top-10 paid sales */
+
+SELECT *
+FROM last_paid_click_nvorobyova
+ORDER BY amount DESC NULLS LAST
+LIMIT 10;
