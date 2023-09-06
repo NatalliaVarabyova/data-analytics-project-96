72f995d2f6af5acf58406b4057c539f16dec74b2
Natallia_Varabyova <133032557+NatalliaVarabyova@users.noreply.github.com>
2023-09-07 02:26:47 +0300

    Step 4. Continuation

--- dashboard.sql
+++ dashboard.sql
@@ -180,7 +180,32 @@ FROM sessions
 GROUP BY 1, 2, 3
 ORDER BY 3 ASC, 4 DESC;
 
-/* Шаг 4.12. Расчёт количества лидов по дням. Файл daily_leads_count.csv */
+/* Шаг 4.12 Расчёт коэффициента липучести. Файл sticky_factor.csv */
+
+WITH mau AS (
+    SELECT count(DISTINCT visitor_id) AS mau
+    FROM sessions
+),
+
+pre_dau AS (
+    SELECT
+        to_char(visit_date, 'YYYY-MM-DD') AS visit_date,
+        count(DISTINCT visitor_id) AS daily_visitor_count
+    FROM sessions
+    GROUP BY 1
+),
+
+dau AS (
+    SELECT
+        percentile_cont(0.5) WITHIN GROUP (ORDER BY daily_visitor_count) AS dau
+    FROM pre_dau
+)
+
+SELECT dau.dau * 100.0 / mau.mau AS sticky_factor
+FROM dau, mau;
+
+
+/* Шаг 4.13. Расчёт количества лидов по дням. Файл daily_leads_count.csv */
 
 SELECT
     to_char(created_at, 'YYYY-MM-DD') AS created_at,
@@ -189,7 +214,7 @@ FROM leads
 GROUP BY 1
 ORDER BY 1;
 
-/* Шаг 4.13 Расчёт количества лидов в неделю. Файл weekly_leads_count.csv*/
+/* Шаг 4.14 Расчёт количества лидов в неделю. Файл weekly_leads_count.csv*/
 
 SELECT
     CASE
@@ -213,7 +238,7 @@ FROM leads
 GROUP BY 1
 ORDER BY 1 ASC;
 
-/* Шаг 4.14. Расчёт количества лидов по источникам за июнь. Файл monthly_leads_count_from_source.csv */
+/* Шаг 4.15. Расчёт количества лидов по источникам за июнь. Файл monthly_leads_count_from_source.csv */
 
 SELECT
     s.source,
@@ -224,7 +249,7 @@ INNER JOIN sessions AS s
 GROUP BY 1
 ORDER BY 2 DESC;
 
-/* Шаг 4.15. Расчёт количества лидов по источникам по дням. Файл daily_leads_count_from_source.csv */
+/* Шаг 4.16. Расчёт количества лидов по источникам по дням. Файл daily_leads_count_from_source.csv */
 
 SELECT
     s.source,
@@ -236,7 +261,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2
 ORDER BY 2 ASC, 3 DESC;
 
-/* Шаг 4.16. Расчёт количества лидов по источникам в неделю. Файл weekly_leads_count_from_source.csv */
+/* Шаг 4.17. Расчёт количества лидов по источникам в неделю. Файл weekly_leads_count_from_source.csv */
 
 SELECT
     s.source,
@@ -263,7 +288,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2
 ORDER BY 2 ASC, 3 DESC;
 
-/* Шаг 4.17. Расчёт количества лидов по source / medium / campaign за июнь. Файл monthly_leads_count_from_campaign.csv */
+/* Шаг 4.18. Расчёт количества лидов по source / medium / campaign за июнь. Файл monthly_leads_count_from_campaign.csv */
 
 SELECT
     s.source,
@@ -276,7 +301,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2, 3
 ORDER BY 4 DESC;
 
-/* Шаг 4.18. Расчёт количества лидов по source / medium / campaign по дням. Файл daily_leads_count_from_campaign.csv */
+/* Шаг 4.19. Расчёт количества лидов по source / medium / campaign по дням. Файл daily_leads_count_from_campaign.csv */
 
 SELECT
     s.source,
@@ -290,7 +315,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2, 3, 4
 ORDER BY 4 ASC, 5 DESC;
 
-/* Шаг 4.19. Расчёт количества лидов по source / medium / campaign в неделю. Файл weekly_leads_count_from_campaign.csv */
+/* Шаг 4.20. Расчёт количества лидов по source / medium / campaign в неделю. Файл weekly_leads_count_from_campaign.csv */
 
 SELECT
     s.source,
@@ -319,7 +344,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2, 3, 4
 ORDER BY 4 ASC, 5 DESC;
 
-/* Шаг 4.20. Расчёт количества лидов по source / medium за июнь. Файл monthly_leads_count_from_medium.csv */
+/* Шаг 4.21. Расчёт количества лидов по source / medium за июнь. Файл monthly_leads_count_from_medium.csv */
 
 SELECT
     s.source,
@@ -334,7 +359,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
-/* Шаг 4.21. Расчёт количества лидов по source / medium по дням. Файл daily_leads_count_from_medium.csv */
+/* Шаг 4.22. Расчёт количества лидов по source / medium по дням. Файл daily_leads_count_from_medium.csv */
 
 SELECT
     s.source,
@@ -350,7 +375,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2, 3
 ORDER BY 3 ASC, 4 DESC;
 
-/* Шаг 4.22. Расчёт количества лидов по source / medium в неделю. Файл weekly_leads_count_from_medium.csv */
+/* Шаг 4.23. Расчёт количества лидов по source / medium в неделю. Файл weekly_leads_count_from_medium.csv */
 
 SELECT
     s.source,
@@ -381,7 +406,7 @@ INNER JOIN sessions AS s
 GROUP BY 1, 2, 3
 ORDER BY 3 ASC, 4 DESC;
 
-/* Шаг 4.23. Расчёт конверсии из клика в лид по source. Файл lcr_from_source.csv */
+/* Шаг 4.24. Расчёт конверсии из клика в лид по source. Файл lcr_from_source.csv */
 
 SELECT
     s.source,
@@ -394,7 +419,7 @@ LEFT JOIN leads AS l
 GROUP BY 1
 ORDER BY 2 DESC;
 
-/* Шаг 4.24. Расчёт конверсии из клика в лид по source / medium. Файл lcr_from_medium.csv */
+/* Шаг 4.25. Расчёт конверсии из клика в лид по source / medium. Файл lcr_from_medium.csv */
 
 SELECT
     s.source,
@@ -408,7 +433,7 @@ LEFT JOIN leads AS l
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
-/* Шаг 4.25. Расчёт конверсии из клика в лид по source / medium / campaign. Файл lcr_from_campaign.csv */
+/* Шаг 4.26. Расчёт конверсии из клика в лид по source / medium / campaign. Файл lcr_from_campaign.csv */
 
 SELECT
     s.source,
@@ -423,7 +448,7 @@ LEFT JOIN leads AS l
 GROUP BY 1, 2, 3
 ORDER BY 4 DESC;
 
-/* Шаг 4.26. Расчёт конверсии из лида в клиента по source. Файл lead_client_from_source.csv */
+/* Шаг 4.27. Расчёт конверсии из лида в клиента по source. Файл lead_client_from_source.csv */
 
 SELECT
     s.source,
@@ -442,7 +467,8 @@ GROUP BY 1
 HAVING count(l.lead_id) > 0
 ORDER BY 2 DESC;
 
-/* Шаг 4.27. Расчёт конверсии из лида в клиента по source / medium. Файл lead_client_from_medium.csv */
+
+/* Шаг 4.28. Расчёт конверсии из лида в клиента по source / medium. Файл lead_client_from_medium.csv */
 
 SELECT
     s.source,
@@ -462,7 +488,7 @@ GROUP BY 1, 2
 HAVING count(l.lead_id) > 0
 ORDER BY 3 DESC;
 
-/* Шаг 4.28. Расчёт конверсии из лида в клиента по source / medium. Файл lead_client_from_campaign.csv */
+/* Шаг 4.29. Расчёт конверсии из лида в клиента по source / medium. Файл lead_client_from_campaign.csv */
 
 SELECT
     s.source,
@@ -482,3 +508,155 @@ LEFT JOIN leads AS l
 GROUP BY 1, 2, 3
 HAVING count(l.lead_id) > 0
 ORDER BY 4 DESC;
+
+/* Шаг 4.30. Расчёт расходов на рекламу в динамике. Файл ad_costs.csv */
+
+WITH vk_costs AS (
+    SELECT
+        to_char(campaign_date, 'YYYY-MM-DD') AS campaign_date,
+        sum(daily_spent) AS vk_costs
+    FROM vk_ads
+    GROUP BY 1
+),
+
+ya_costs AS (
+    SELECT
+        to_char(campaign_date, 'YYYY-MM-DD') AS campaign_date,
+        sum(daily_spent) AS ya_costs
+    FROM ya_ads
+    GROUP BY 1
+)
+
+SELECT
+    vk_costs.campaign_date,
+    vk_costs.vk_costs,
+    ya_costs.ya_costs,
+    vk_costs.vk_costs + ya_costs.ya_costs AS total_costs
+FROM vk_costs
+LEFT JOIN ya_costs
+    ON vk_costs.campaign_date = ya_costs.campaign_date
+ORDER BY 1;
+
+/* Шаг 4.31. Расчёт расходов на рекламу в динамике по source / medium / campaign. Файл ad_costs_campaign.csv */
+
+SELECT
+    to_char(campaign_date, 'YYYY-MM-DD') AS campaign_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    sum(daily_spent) AS vk_costs
+FROM vk_ads
+GROUP BY 1, 2, 3, 4
+UNION
+SELECT
+    to_char(campaign_date, 'YYYY-MM-DD') AS campaign_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    sum(daily_spent) AS ya_costs
+FROM ya_ads
+GROUP BY 1, 2, 3, 4
+ORDER BY 1, 2, 3, 4;
+
+/* Шаг 4.32. Расчёт CPU по атрибуции last paid click. Файл monthly_cpu.scv */
+
+SELECT
+    max(CASE
+        WHEN vk_daily_spent IS NOT null THEN vk_daily_spent
+        WHEN ya_daily_spent IS NOT null THEN ya_daily_spent
+        ELSE 0
+    END) * 1.0 / count(DISTINCT visitor_id) AS monthly_cpu
+FROM last_paid_costs_nv;
+
+/* Шаг 4.33. Расчёт CPU по неделям по атрибуции last paid click. Файл weekly_cpu.scv */
+
+SELECT
+    CASE
+        WHEN to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-07' THEN '1 неделя'
+        WHEN
+            to_char(visit_date, 'YYYY-MM-DD') >= '2023-06-08'
+            AND to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-14'
+            THEN '2 неделя'
+        WHEN
+            to_char(visit_date, 'YYYY-MM-DD') >= '2023-06-15'
+            AND to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-21'
+            THEN '3 неделя'
+        WHEN
+            to_char(visit_date, 'YYYY-MM-DD') >= '2023-06-22'
+            AND to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-28'
+            THEN '4 неделя'
+        ELSE '5 неделя (2 дня)'
+    END AS week_of_june,
+    max(CASE
+        WHEN vk_daily_spent IS NOT null THEN vk_daily_spent
+        WHEN ya_daily_spent IS NOT null THEN ya_daily_spent
+        ELSE 0
+    END) * 1.0 / count(DISTINCT visitor_id) AS cpu
+FROM last_paid_costs_nv
+GROUP BY 1;
+
+/* Шаг 4.34. Расчёт CPU по дням по атрибуции last paid click. Файл daily_cpu.scv */
+
+SELECT
+    to_char(visit_date, 'YYYY-MM-DD') AS visit_date,
+    max(CASE
+        WHEN vk_daily_spent IS NOT null THEN vk_daily_spent
+        WHEN ya_daily_spent IS NOT null THEN ya_daily_spent
+        ELSE 0
+    END) * 1.0 / count(DISTINCT visitor_id) AS cpu
+FROM last_paid_costs_nv
+GROUP BY 1
+ORDER BY 1;
+
+/* Шаг 4.35. Расчёт CPL по атрибуции last paid click. Файл monthly_cpl.scv */
+
+SELECT
+    max(CASE
+        WHEN vk_daily_spent IS NOT null THEN vk_daily_spent
+        WHEN ya_daily_spent IS NOT null THEN ya_daily_spent
+        ELSE 0
+    END) * 1.0 / count(DISTINCT lead_id) AS monthly_cpl
+FROM last_paid_costs_nv;
+
+/* Шаг 4.36. Расчёт CPL по неделям по атрибуции last paid click. Файл weekly_cpl.scv */
+
+SELECT
+    CASE
+        WHEN to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-07' THEN '1 неделя'
+        WHEN
+            to_char(visit_date, 'YYYY-MM-DD') >= '2023-06-08'
+            AND to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-14'
+            THEN '2 неделя'
+        WHEN
+            to_char(visit_date, 'YYYY-MM-DD') >= '2023-06-15'
+            AND to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-21'
+            THEN '3 неделя'
+        WHEN
+            to_char(visit_date, 'YYYY-MM-DD') >= '2023-06-22'
+            AND to_char(visit_date, 'YYYY-MM-DD') <= '2023-06-28'
+            THEN '4 неделя'
+        ELSE '5 неделя (2 дня)'
+    END AS week_of_june,
+    round(max(CASE
+        WHEN vk_daily_spent IS NOT null THEN vk_daily_spent
+        WHEN ya_daily_spent IS NOT null THEN ya_daily_spent
+        ELSE 0
+    END) * 1.0 / count(DISTINCT lead_id), 2) AS cpl
+FROM last_paid_costs_nv
+GROUP BY 1;
+
+/* Шаг 4.37. Расчёт CPL по дням по атрибуции last paid click. Файл daily_cpl.scv */
+
+SELECT
+    to_char(visit_date, 'YYYY-MM-DD') AS visit_date,
+    round(max(CASE
+        WHEN vk_daily_spent IS NOT null THEN vk_daily_spent
+        WHEN ya_daily_spent IS NOT null THEN ya_daily_spent
+        ELSE 0
+    END) * 1.0 / (count(DISTINCT lead_id)), 2) AS cpl
+FROM last_paid_costs_nv
+GROUP BY 1
+HAVING count(DISTINCT lead_id) > 0
+ORDER BY 1;
+
+
